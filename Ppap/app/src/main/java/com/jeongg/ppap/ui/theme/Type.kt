a4c12041d402147bc83b64cd7df5a8a858a758cd
WoeunJung <leena09123@naver.com>
2023-08-12 16:46:04 +0900

    design: add Typography and fonts(Pretendard)

--- Ppap/app/src/main/java/com/jeongg/ppap/ui/theme/Type.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/ui/theme/Type.kt
@@ -2,33 +2,69 @@ package com.jeongg.ppap.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jeongg.ppap.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+val RobotoFamily = FontFamily(
+    Font(R.font.bold),
+    Font(R.font.extra_bold),
+    Font(R.font.extra_light),
+    Font(R.font.light),
+    Font(R.font.medium),
+    Font(R.font.pretendard_black),
+    Font(R.font.regular),
+    Font(R.font.semi_bold),
+    Font(R.font.thin),
+)
+
+val typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 50.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleLarge = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+    ),
+    displayLarge = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = RobotoFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 15.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
     )
-    */
 )
