3d1316137e801c62e6838226e3a1e38d42b28cd7
WoeunJung <leena09123@naver.com>
2023-08-15 22:16:15 +0900

    #5 refactor: remove unused imports and seperate to string resource
    - 사용하지 않는 import 지우기
    - 문자열 다 string.xml로 분리하기

--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/notice/NoticeListScreen.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/notice/NoticeListScreen.kt
@@ -1,6 +1,5 @@
 package com.jeongg.ppap.presentation.notice
 
-import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,14 +8,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -31,19 +28,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.jeongg.ppap.R
 import com.jeongg.ppap.presentation.component.PDivider
 import com.jeongg.ppap.presentation.navigation.Screen
-import com.jeongg.ppap.presentation.subscribe.ImageDescription
-import com.jeongg.ppap.presentation.subscribe.TextDescription
 import com.jeongg.ppap.ui.theme.Dimens
 import com.jeongg.ppap.ui.theme.bright_pink
 import com.jeongg.ppap.ui.theme.bright_yellow
 import com.jeongg.ppap.ui.theme.main_green
-import com.jeongg.ppap.ui.theme.main_yellow
 import com.jeongg.ppap.ui.theme.shapes
 
 @Composable
@@ -55,10 +48,10 @@ fun NoticeListScreen(
             .padding(Dimens.PaddingNormal)
             .fillMaxSize()
     ){
-        NoticeListTitle()
+        NoticeListTitle(bookmarkNavigate = {navController.navigate(Screen.NoticeScrapScreen.route)})
         PDivider(modifier = Modifier.padding(top = 7.dp, bottom = 15.dp))
         LazyColumn {
-            item { NoticeListBanner() }
+            item { NoticeListBanner(navController) }
             repeat(15){
                 item { NoticeItem() }
             }
@@ -68,12 +61,14 @@ fun NoticeListScreen(
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NoticeListBanner() {
+fun NoticeListBanner(
+    navController: NavController
+) {
     val state = rememberPagerState(initialPage = 0) { 3 }
     val colors = listOf(bright_yellow, bright_pink, main_green)
     val images = listOf(R.drawable.pineapple, R.drawable.apple_no_background, R.drawable.pineapple)
-    val titles = listOf("스크랩 화면 바로가기", "공지 추가 등록하기", "구독한 공지사항 수정하기")
-    val descriptions = listOf("하트를 눌러서 공지사항을 저장하세요", "학과 공지사항 등 필요한 공지를 등록해주세요.", "알림 여부, 공지 이름 등을 수정해보세요.")
+    val titles = listOf(R.string.banner_title1, R.string.banner_title2, R.string.banner_title3)
+    val descriptions = listOf(R.string.banner_description1,R.string.banner_description2, R.string.banner_description3)
     val screens = listOf(Screen.SubscribeScreen.route, Screen.SubscribeAddScreen.route, Screen.SubscribeScreen.route)
 
     HorizontalPager(
@@ -83,7 +78,7 @@ fun NoticeListBanner() {
         Column(
             modifier = Modifier
                 .clip(shapes.small)
-                .clickable{ }
+                .clickable { navController.navigate(screens[index]) }
                 .fillMaxWidth()
                 .defaultMinSize(minHeight = 62.dp)
                 .background(colors[index])
@@ -94,16 +89,18 @@ fun NoticeListBanner() {
                 Image(
                     painter = painterResource(images[index]),
                     contentDescription = "character",
-                    modifier = Modifier.padding(end = 15.dp).height(44.dp)
+                    modifier = Modifier
+                        .padding(end = 15.dp)
+                        .height(44.dp)
                 )
                 Column {
                     Text(
-                        text = titles[index],
+                        text = stringResource(titles[index]),
                         style = MaterialTheme.typography.displayLarge,
                         color = Color.Black
                     )
                     Text(
-                        text = descriptions[index],
+                        text = stringResource(descriptions[index]),
                         style = MaterialTheme.typography.labelLarge,
                         color = Color.Black
                     )
@@ -115,7 +112,7 @@ fun NoticeListBanner() {
                 horizontalArrangement = Arrangement.Center
             ) {
                 repeat(3) { iteration ->
-                    val color = if (state.currentPage == iteration) Color.White else Color.White.copy(alpha = 0.5f)
+                    val color = Color.White.copy(alpha = if(state.currentPage == iteration) 1f else 0.5f)
                     Box(
                         modifier = Modifier
                             .padding(horizontal = 2.dp, vertical = 5.dp)
@@ -131,7 +128,10 @@ fun NoticeListBanner() {
 }
 
 @Composable
-fun NoticeListTitle(){
+fun NoticeListTitle(
+    bookmarkNavigate: () -> Unit = {},
+    settingNavigate: () -> Unit = {}
+){
     Box(
         modifier = Modifier.fillMaxWidth()
     ) {
@@ -146,12 +146,12 @@ fun NoticeListTitle(){
             Image(
                 painter = painterResource(R.drawable.bookmark),
                 contentDescription = "bookmark",
-                modifier = Modifier.clickable { }
+                modifier = Modifier.clickable(onClick = bookmarkNavigate)
             )
             Icon(
                 painter = painterResource(R.drawable.setting),
                 contentDescription = "setting",
-                modifier = Modifier.clickable { },
+                modifier = Modifier.clickable(onClick = settingNavigate),
                 tint = MaterialTheme.colorScheme.outline
             )
         }
