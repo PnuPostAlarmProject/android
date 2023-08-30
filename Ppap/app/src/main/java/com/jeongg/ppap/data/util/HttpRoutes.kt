ab88d49dd0e1ead37f2e223f8f0b4a9d2d26e895
WoeunJung <leena09123@naver.com>
2023-08-30 19:23:43 +0900

    fix: network logic add ContentEnconding, log, and subscribe DI
    - contentEncoding 추가
    - refreshToken, accessToken 변수로 빼두지 않기(나중에 사용하려고 하면 null로 들어가기 때문)
    - accept는 자동으로 JSON으로 들어가므로 필요없는 코드
    - BASE_URL 지정

--- Ppap/app/src/main/java/com/jeongg/ppap/data/util/HttpRoutes.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/data/util/HttpRoutes.kt
@@ -1,9 +1,15 @@
 package com.jeongg.ppap.data.util
 
 object HttpRoutes {
-    private const val BASE_URL = "http://192.168.219.100:8080"
-    const val KAKAO_LOGIN = "$BASE_URL/auth/kakao/login"
-    const val KAKAO_REISSUE = "$BASE_URL/auth/reissue"
-    const val LOGOUT = "$BASE_URL/auth/logout"
-    const val WITHDRAWL = "$BASE_URL/auth/withdrawl"
+    const val BASE_URL = "http://192.168.219.100:8080"
+    const val KAKAO_LOGIN = "/auth/kakao/login"
+    const val KAKAO_REISSUE = "/auth/reissue"
+    const val LOGOUT = "/auth/logout"
+    const val WITHDRAWL = "/auth/withdrawl"
+    const val CREATE_SUBSCRIBE = "/subscribe"
+    const val GET_SUBSCRIBES = "/subscribe"
+    const val GET_SUBSCRIBE = "/subscribe"
+    const val UPDATE_SUBSCRIBE = "/subscribe"
+    const val DELETE_SUBSCRIBE = "/subscribe/delete"
+    const val UPDATE_ACTIVE = "/subscribe/active"
 }
