aaf0035d73497f62f30642d65ed873b2a9f70893
WoeunJung <leena09123@naver.com>
2024-01-19 23:01:37 +0900

    #30 design: update login, splash Screen UI

--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/login/LoginScreen.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/login/LoginScreen.kt
@@ -3,11 +3,11 @@ package com.jeongg.ppap.presentation.login
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -53,45 +54,66 @@ fun LoginScreen(
         modifier = Modifier.padding(Dimens.PaddingNormal)
     ){
         Column {
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(top = Dimens.PaddingExtraLarge)
+            Image(
+                painter = painterResource(id = R.drawable.name_yellow),
+                contentDescription = "PPAP",
+                modifier = Modifier.padding(top = Dimens.PaddingExtraLarge).width(117.dp)
             )
-            PDivider(modifier = Modifier.padding(vertical = Dimens.PaddingSmall))
-            Box(
-                modifier = Modifier.fillMaxWidth()
-            ){
-                Text(
-                    text = stringResource(R.string.app_description),
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(end = 100.dp)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.kakao_login),
-                    contentDescription = stringResource(R.string.login),
-                    modifier = Modifier
-                        .width(100.dp)
-                        .height(50.dp)
-                        .align(Alignment.CenterEnd)
-                        .clickable { viewModel.kakaoLogin(context) }
-                )
-            }
+            PDivider(
+                modifier = Modifier.padding(vertical = Dimens.PaddingSmall)
+            )
+            KakaoLogin(
+                onClick = { viewModel.kakaoLogin(context) }
+            )
         }
-        Row(
+        LoginCharacters(
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
+
+@Composable
+private fun LoginCharacters(
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier.size(200.dp),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pineapple),
+            contentDescription = stringResource(R.string.characters),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.apple),
+            contentDescription = stringResource(R.string.characters),
+        )
+    }
+}
+
+@Composable
+private fun KakaoLogin(
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.app_description),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(end = 100.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.kakao_login),
+            contentDescription = stringResource(R.string.login),
             modifier = Modifier
-                .size(200.dp)
-                .align(Alignment.BottomEnd),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.pineapple),
-                contentDescription = stringResource(R.string.characters),
-            )
-            Image(
-                painter = painterResource(id = R.drawable.apple_no_background),
-                contentDescription = stringResource(R.string.characters),
-            )
-        }
+                .width(95.dp)
+                .align(Alignment.CenterEnd)
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = onClick
+                )
+        )
     }
 }
--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/splash/SplashScreen.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/splash/SplashScreen.kt
@@ -1,31 +1,25 @@
 package com.jeongg.ppap.presentation.splash
 
-import androidx.annotation.DrawableRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -33,7 +27,6 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -47,34 +40,11 @@ fun SplashScreen(
     navController: NavController,
     viewModel: SplashViewModel = hiltViewModel()
 ){
-    var pineapple by remember { mutableStateOf(false) }
-    var apple by remember { mutableStateOf(false) }
-    var pencil by remember { mutableStateOf(false) }
-    var fadeOut by remember { mutableStateOf(true) }
-    var fadeIn by remember { mutableStateOf(false) }
-
     val animation = remember { Animatable(initialValue = 0f) }
-    val distance = with(LocalDensity.current){ 180.dp.toPx() }
-    LaunchedEffect(key1 = true){
-        delay(400)
-        apple = true
-        delay(400)
-        pineapple = true
-        delay(700)
-        pencil = true
-        animation.animateTo(
-            targetValue = 1f,
-            animationSpec = keyframes {
-                durationMillis = 400
-                0.0f at 0 with LinearOutSlowInEasing
-                1.0f at 200 with LinearOutSlowInEasing
-            }
-        )
-        fadeOut = false
-        delay(500)
-        fadeIn = true
-        delay(300)
+    val distance = with(LocalDensity.current){ 100.dp.toPx() }
 
+    LaunchedEffect(key1 = true){
+        splashAnimate(animation)
         val nextScreen = when (viewModel.isUserLoggedIn()) {
             true -> Screen.NoticeListScreen
             false -> Screen.LoginScreen
@@ -82,86 +52,112 @@ fun SplashScreen(
         navController.popBackStack()
         navController.navigate(nextScreen.route)
     }
+
     Box(
         modifier = Modifier.fillMaxSize()
     ){
-        ScaleAnimation(
-            isVisible = fadeOut
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(70.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (pencil) {
-                Image(
-                    painter = painterResource(R.drawable.pencil),
-                    contentDescription = "pencil",
-                    modifier = Modifier
-                        .padding(bottom = 200.dp)
-                        .align(Alignment.CenterEnd)
-                        .graphicsLayer {
-                            translationX = -animation.value * distance
-                            translationY = animation.value * distance
-                        }
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .height(200.dp)
-                    .align(Alignment.Center),
-                verticalAlignment = Alignment.Bottom
-            ) {
-                FadeInAnimation(apple, R.drawable.apple_no_background)
-                FadeInAnimation(pineapple, R.drawable.pineapple)
-            }
-        }
-        ScaleAnimation(
-            isVisible = fadeIn
-        ) {
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.align(Alignment.Center),
-                textAlign = TextAlign.Center
+            SplashTitle()
+            SplashCharacters(
+                modifier = Modifier.graphicsLayer {
+                    translationX = -animation.value * distance
+                }
             )
         }
-       Text(
-           text = "@" + stringResource(R.string.team_name),
-           style = MaterialTheme.typography.titleSmall.copy(
-               brush = Brush.linearGradient(rainbowColorsBrush)
-           ),
-           modifier = Modifier
-               .padding(15.dp)
-               .align(Alignment.BottomCenter),
-       )
+        Footer(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 }
 
+suspend fun splashAnimate(animation: Animatable<Float, AnimationVector1D>) {
+    delay(200)
+    animation.animateTo(
+        targetValue = 1f,
+        animationSpec = keyframes {
+            durationMillis = 300
+            0.0f at 0 with LinearOutSlowInEasing
+            1.0f at 1000 with LinearOutSlowInEasing
+        }
+    )
+    delay(100)
+}
+
 @Composable
-fun ScaleAnimation(
-    isVisible: Boolean = true,
-    content: @Composable (BoxScope.() -> Unit),
-){
-    AnimatedVisibility(
-        visible = isVisible,
-        enter = scaleIn() + expandVertically(expandFrom = Alignment.CenterVertically),
-        exit = fadeOut(animationSpec = tween(200)) + scaleOut(),
-        modifier = Modifier.fillMaxSize(),
-    ){
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            content = content
+fun SplashTitle(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.name_yellow),
+            contentDescription = "PPAP",
+            modifier = Modifier.padding(bottom = 25.dp).width(117.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.name_description),
+            contentDescription = "Pusan Post Alarm Project",
+            modifier = Modifier.width(135.dp)
         )
     }
 }
 @Composable
-fun FadeInAnimation(
-    isVisible: Boolean = true,
-    @DrawableRes id: Int = R.drawable.pineapple
+fun SplashCharacters(
+    modifier: Modifier
+){
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        PenAnimation(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            animationModifier = modifier.size(200.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.pineapple),
+                contentDescription = "pineapple character",
+                modifier = Modifier.width(146.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.apple),
+                contentDescription = "apple character",
+                modifier = Modifier.width(66.dp)
+            )
+        }
+
+    }
+}
+@Composable
+fun PenAnimation(
+    modifier: Modifier,
+    animationModifier: Modifier
 ){
     AnimatedVisibility(
-        visible = isVisible,
-        enter = fadeIn(animationSpec = tween(durationMillis = 1000))
+        visible = true,
+        modifier = modifier
     ) {
         Image(
-            painter = painterResource(id = id),
-            contentDescription = "animation",
+            painter = painterResource(R.drawable.pen),
+            contentDescription = "pen",
+            modifier = animationModifier
         )
     }
+}
+@Composable
+fun Footer(
+    modifier: Modifier
+){
+    Text(
+        text = "@" + stringResource(R.string.team_name),
+        style = MaterialTheme.typography.titleSmall.copy(
+            brush = Brush.linearGradient(rainbowColorsBrush)
+        ),
+        modifier = modifier.padding(15.dp)
+    )
 }
