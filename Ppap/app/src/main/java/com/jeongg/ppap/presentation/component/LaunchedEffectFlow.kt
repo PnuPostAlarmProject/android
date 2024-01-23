78d739b9cf120f6f12a18507f5ec91935d2b13b1
WoeunJung <leena09123@naver.com>
2024-01-23 22:59:46 +0900

    #32 feat: seperate eventFlow handling and update event contents

---
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/component/LaunchedEffectFlow.kt
@@ -0,0 +1,28 @@
+package com.jeongg.ppap.presentation.component
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import com.jeongg.ppap.presentation.util.PEvent
+import com.jeongg.ppap.util.log
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun LaunchedEffectEvent(
+    eventFlow: SharedFlow<PEvent>,
+    onNavigate: () -> Unit = {},
+) {
+    val context = LocalContext.current
+    LaunchedEffect(key1 = true) {
+        eventFlow.collectLatest { event ->
+            when (event) {
+                is PEvent.NAVIGATE -> onNavigate()
+                is PEvent.TOAST -> Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                is PEvent.LOADING -> { "로딩중".log() }
+                is PEvent.SUCCESS -> { }
+            }
+        }
+    }
+}
