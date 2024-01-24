baca6bae4e94afd8afa372724989757a97ca4b98
WoeunJung <leena09123@naver.com>
2024-01-24 21:55:14 +0900

    #32 design: tiny ui changes

--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/component/PTitle.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/component/PTitle.kt
@@ -1,41 +1,32 @@
 package com.jeongg.ppap.presentation.component
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import com.jeongg.ppap.R
 import com.jeongg.ppap.theme.Dimens
-import com.jeongg.ppap.theme.gray3
 
 @Composable
 fun PTitle(
     modifier: Modifier = Modifier,
     title: String = "제목",
-    content: @Composable (ColumnScope.() -> Unit),
+    content: LazyListScope.() -> Unit,
 ){
-    Column(
+    LazyColumn(
         modifier = modifier
             .padding(Dimens.ScreenPadding)
             .fillMaxSize()
     ){
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleLarge,
-        )
+        item {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
         content()
     }
 }
--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/subscribe_add/SubscribeAddEvent.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/presentation/subscribe_add/SubscribeAddEvent.kt
@@ -2,7 +2,6 @@ package com.jeongg.ppap.presentation.subscribe_add
 
 sealed class SubscribeAddEvent{
     data class EnteredTitle(val title: String): SubscribeAddEvent()
-    data class EnteredNoticeLink(val notice: String?): SubscribeAddEvent()
     data class EnteredRssLink(val rss: String): SubscribeAddEvent()
     object SaveSubscribe: SubscribeAddEvent()
 }
