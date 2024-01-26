3746c6b6625a3dd763f8c5a008da91b7554bb5d5
WoeunJung <leena09123@naver.com>
2024-01-27 01:46:21 +0900

    #34 chore: update build grade into 8.2.2

--- Ppap/app/build.gradle.kts
+++ Ppap/app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0-rc01")
 
     // navigation
     implementation("androidx.navigation:navigation-common-ktx:2.7.6")
