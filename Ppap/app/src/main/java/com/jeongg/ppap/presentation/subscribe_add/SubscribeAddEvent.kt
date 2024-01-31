c1d22343bcebfe8ebf9741427db9ef0220a14257
WoeunJung <leena09123@naver.com>
2024-02-01 00:19:58 +0900

    #37 refactor: change subscribeAdd -> subscribeCustomAdd

--- Ppap/app/src/main/java/com/jeongg/ppap/presentation/subscribe_add/SubscribeAddEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.jeongg.ppap.presentation.subscribe_add
-
-sealed class SubscribeAddEvent{
-    data class EnteredTitle(val title: String): SubscribeAddEvent()
-    data class EnteredRssLink(val rss: String): SubscribeAddEvent()
-    object SaveSubscribe: SubscribeAddEvent()
-}
