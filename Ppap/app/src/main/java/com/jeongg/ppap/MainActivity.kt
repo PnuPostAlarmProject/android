81517d63edc1f5eedc544f14dad9f741332e1fac
WoeunJung <leena09123@naver.com>
2024-02-08 23:56:49 +0900

    #41 feat: get alarm notification permission when add subscribes

--- Ppap/app/src/main/java/com/jeongg/ppap/MainActivity.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/MainActivity.kt
@@ -2,17 +2,12 @@ package com.jeongg.ppap
 
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.preferencesDataStore
 import com.jeongg.ppap.presentation.PpapAppTheme
-import com.jeongg.ppap.util.log
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -23,7 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         openNoticeLink()
-        askNotificationPermission()
         setContent { PpapAppTheme() }
     }
 
@@ -34,25 +28,6 @@ class MainActivity : ComponentActivity() {
                 val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
                 startActivity(browserIntent)
             }
-        } catch(_: Exception){
-            "알림 링크 열기 실패".log()
-        }
+        } catch(_: Exception){ }
     }
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { }
-
-    private fun askNotificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-
-            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
-
-            } else {
-                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
-
 }
