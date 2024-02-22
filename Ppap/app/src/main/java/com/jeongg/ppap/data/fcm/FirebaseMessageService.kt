3c20f74606ad17601bfc0f0e1484d40ecbe705bf
WoeunJung <leena09123@naver.com>
2024-02-22 15:47:22 +0900

    feat: FCM send message in foreground

--- Ppap/app/src/main/java/com/jeongg/ppap/data/fcm/FirebaseMessageService.kt
+++ Ppap/app/src/main/java/com/jeongg/ppap/data/fcm/FirebaseMessageService.kt
@@ -1,6 +1,10 @@
 package com.jeongg.ppap.data.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import androidx.core.app.NotificationCompat
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.google.android.gms.tasks.OnCompleteListener
@@ -8,6 +12,7 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.jeongg.ppap.MainActivity
+import com.jeongg.ppap.R
 import com.jeongg.ppap.data._const.DataStoreKey
 import com.jeongg.ppap.dataStore
 import kotlinx.coroutines.runBlocking
@@ -17,9 +22,11 @@ class FirebaseMessageService: FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         if (message.notification != null){
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+            sendNotification(
+                message.notification?.title,
+                message.notification?.body,
+                message.data["link"]
+            )
         }
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
@@ -34,6 +41,34 @@ class FirebaseMessageService: FirebaseMessagingService() {
             dataStore.edit{ it[key] = token }
         }
         super.onNewToken(token)
+    }
+
+    private fun sendNotification(title: String?, body: String?, link: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra("link", link)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val channelId = "ppap channel id"
+        val notificationBuilder: NotificationCompat.Builder =
+            NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(
+            channelId,
+            "ppap channel name",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
 }
