36d9f05a8d91721d97e2224843d216421569d99a
WoeunJung <leena09123@naver.com>
2024-02-23 20:43:33 +0900

    chore: update application version

--- Ppap/app/build.gradle.kts
+++ Ppap/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.jeongg.ppap"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
 
